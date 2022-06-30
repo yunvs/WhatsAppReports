b8b2e65c6f990abc2f7837a5a24c9d0f190c4a57
Yunus R <73846253+yunvs@users.noreply.github.com>
2022-06-30 21:32:02 +0200

    added more functions

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from myfunctions import *
 
 
-paths = ["data/chat_small.txt", "data/chat_mum.txt", "data/chat_mum.zip", "tash.trash"]
-path_to_file = paths[0] # 0: small, 1: mum.zip, 2: mum.txt, 3: trash
+paths = ["data/chat_small.txt", "data/chat_mum.txt", "data/chat_mum.zip", "tash.trash", "data/chat_AB.txt"]
+path_to_file = paths[2] # 0: small, 1: mum.zip, 2: mum.txt, 3: trash, 4: AB
 
-contents = get_content(path_to_file)
-chat_history = chats_to_list(contents)
+contents = get_content(path_to_file) # get the content of the filepath
+chat_history = chats_to_list(contents) # convert the chat history to a list of all messages 
+# Format: [ [[date, time], sender, message], [[date, time], sender, message], … ]
+sender_texts = chats_per_sender(chat_history) # divide the chat history into a list of lists, where each list contains all messages from a sender # [ [all messages from first sender], [all messages from next sender], … ]
 
-print(len(chat_history))
+chats_per_senders = chats_per_sender(chat_history)
+amount_senders = len(chats_per_senders)
+
+for i in range(amount_senders):
+    globals()['chats_sender'+str(i)] = chats_per_senders[i]
 
 
-chats_senders = chats_per_sender(chat_history)
-# print(chats_senders[0])
-for a in chats_senders[0]:
-    if not a.startswith('m'):
-        print("fuck")
-for a in chats_senders[1]:
-    if not a.startswith('y'):
-        print("fuck")
+print(chats_per_senders[0][0])
+print(chats_per_senders[1][0])
 
-print(len(chats_senders[0])+len(chats_senders[1]))
+
+
+
+
+
+
+
+
+print(f"")
--- myfunctions.py
+++ myfunctions.py
@@ -1,7 +1,7 @@
 import re, os, sys, zipfile
+import pandas as pd
 
 PATTERN = r"^\u200E?\[(\d{1,2}.\d{1,2}.\d{2}), (\d{2}:\d{2}:\d{2})\] \u200E?(\b[\w ]*\b): (.*)$"
-
 
 
 def get_content(path):
@@ -9,7 +9,7 @@ def get_content(path):
     Gets file from given path, checks for desired fileformat and extracts the
     content of the file.
     """
-    name, extension = os.path.splitext(path)
+    _name, extension = os.path.splitext(path)
     if extension != ".zip" and extension != ".txt":
         sys.exit("ERROR: Sorry, only .txt or .zip files are supported")
     elif extension == ".zip":
@@ -43,28 +43,18 @@ def chats_to_list(string):
     In each message, the first element is a list of the date and time, 
     the second element is the sender, and the third element is the message.
     [[[Date, Time], Sender, Message], [[Date, Time], Sender, Message], … ]
-
     """
     chat_history = list() # list of all messages
     msg_count = 0 # total number of messages
-    sender_texts = list()
-    sender_dict = dict()
 
     for line in string:
         if check_for_pattern(line):
             res = get_groups(line)
             chat_history.append([[res.group(1), res.group(2)], res.group(3), res.group(4)])
             msg_count += 1
-            # if res.group(1) not in sender_dict:
-            #     sender_dict[res.group(1)] = len(sender_dict)
-            #     sender_texts.append([])
-            # sender_texts[sender_dict[res.group(1)]].append(res.group(2))
         else:
             current_line = "\n" + line.strip("\n")
             chat_history[msg_count-1][2] += current_line
-            # for i, sender in enumerate(senders): 
-            #     if last_sender == sender:
-            #         chat_senders[i].append(current_line)
     return chat_history
 
 
@@ -78,6 +68,6 @@ def chats_per_sender(all_chats):
     for msg in all_chats:
         if msg[1] not in sender_dict:
             sender_dict[msg[1]] = len(sender_dict)
-            sender_texts.append([])
+            sender_texts.append([msg[1]])
         sender_texts[sender_dict[msg[1]]].append(msg[2])
     return sender_texts
