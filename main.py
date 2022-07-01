d3e6a6bc98e2afd1eb7643144e3299490e860690
Yunus R <73846253+yunvs@users.noreply.github.com>
2022-07-01 11:27:00 +0200

    little changes on functions

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
 from myfunctions import *
+import pandas as pd
+from timeit import default_timer as timer
+start = timer()
 
-
-paths = ["data/chat_small.txt", "data/chat_mum.txt", "data/chat_mum.zip", "tash.trash", "data/chat_AB.txt"]
+paths = ["data/chat_small.txt", "data/chat_mum.txt", 
+        "data/chat_mum.zip", "tash.trash", "data/chat_AB.txt"]
 path_to_file = paths[2] # 0: small, 1: mum.zip, 2: mum.txt, 3: trash, 4: AB
 
 contents = get_content(path_to_file) # get the content of the filepath
-chat_history = chats_to_list(contents) # convert the chat history to a list of all messages 
-# Format: [ [[date, time], sender, message], [[date, time], sender, message], … ]
-sender_texts = chats_per_sender(chat_history) # divide the chat history into a list of lists, where each list contains all messages from a sender # [ [all messages from first sender], [all messages from next sender], … ]
+chat_df, sender_dict = convert_chat(contents) # convert the chat to a pd.dataframe 
+# (colums:date,time,sender,message) and a dictionary with the messages per sender
+print(chat_df.head(10))
+senders = list(sender_dict.keys()) # get all sender
+sender_count = len(senders)
+print(f"There are {sender_count} different senders in the chat. Which are:")
+for sender in senders: print(sender)
 
-chats_per_senders = chats_per_sender(chat_history)
-amount_senders = len(chats_per_senders)
-
-for i in range(amount_senders):
-    globals()['chats_sender'+str(i)] = chats_per_senders[i]
-
-
-print(chats_per_senders[0][0])
-print(chats_per_senders[1][0])
-
-
-
-
-
-
-
-
-
-print(f"")
+print(f"\n\nAll Code took {timer() - start} seconds to run.") 
--- myfunctions.py
+++ myfunctions.py
@@ -1,8 +1,6 @@
 import re, os, sys, zipfile
 import pandas as pd
 
-PATTERN = r"^\u200E?\[(\d{1,2}.\d{1,2}.\d{2}), (\d{2}:\d{2}:\d{2})\] \u200E?(\b[\w ]*\b): (.*)$"
-
 
 def get_content(path):
     """
@@ -10,64 +8,39 @@ def get_content(path):
     content of the file.
     """
     _name, extension = os.path.splitext(path)
-    if extension != ".zip" and extension != ".txt":
+    if extension != ".zip" and extension != ".txt": # check for fileformat
         sys.exit("ERROR: Sorry, only .txt or .zip files are supported")
-    elif extension == ".zip":
-        with zipfile.ZipFile(path, 'r') as zip_ref:
+    elif extension == ".zip": # extract the content of zip file
+        with zipfile.ZipFile(path, "r") as zip_ref:
             zip_ref.extractall("data")
-        path = "data/_chat.txt"
-    with open(path, 'r', errors='ignore') as f:
+        path = "data/_chat.txt" 
+    with open(path, "r", errors = "ignore") as f: # read the content of the file
         contents = f.readlines()
     return contents
 
 
-def check_for_pattern(string):
+PATTERN = r"^\u200E?\[([\d./]*), ([\d:]*)\] \u200E?([\w ]*): (.*)$"
+def convert_chat(wa_listed):
     """
-    Check if a string matches the pattern and is a the beginning of a new 
-    message: Starts with [mm/dd/yy, hh:mm:ss]
+    Convert the chat history to a pandas DataFrame (with columns: date, time, 
+    sender, message) and into a dictionary with the messages per sender.
     """
-    return re.match(PATTERN, string) is not None
-
-
-def get_groups(string):
-    """
-    Get the groups of a string.
-    Groups are: 1:Date, 2:Time, 3:Sender, 4:Message
-    """
-    return re.search(PATTERN, string)
-
-
-def chats_to_list(string):
-    """
-    Convert a chat history to a list of messages:
-    In each message, the first element is a list of the date and time, 
-    the second element is the sender, and the third element is the message.
-    [[[Date, Time], Sender, Message], [[Date, Time], Sender, Message], … ]
-    """
-    chat_history = list() # list of all messages
-    msg_count = 0 # total number of messages
-
-    for line in string:
-        if check_for_pattern(line):
-            res = get_groups(line)
-            chat_history.append([[res.group(1), res.group(2)], res.group(3), res.group(4)])
-            msg_count += 1
+    chat = list() # list of all messages
+    sender_messages = dict() # dictionary with key: sender, value: list of messages
+    
+    for line in wa_listed:
+        if re.match(PATTERN, line): # check if a new message starts in line
+            res = re.search(PATTERN, line) # get groups of line
+            # groups are: 1:date, 2:time, 3:sender, 4:message
+            chat.append([res.group(1), res.group(2), res.group(3), res.group(4)])
+            if res.group(3) not in sender_messages:
+                sender_messages[res.group(3)] = [res.group(4)]
+            else:
+                sender_messages[res.group(3)].append(res.group(4))
         else:
-            current_line = "\n" + line.strip("\n")
-            chat_history[msg_count-1][2] += current_line
-    return chat_history
-
-
-def chats_per_sender(all_chats):
-    """
-    Divide the chat history into a list of lists, where each list contains all
-    messages from one sender.
-    """
-    sender_texts = list()
-    sender_dict = dict()
-    for msg in all_chats:
-        if msg[1] not in sender_dict:
-            sender_dict[msg[1]] = len(sender_dict)
-            sender_texts.append([msg[1]])
-        sender_texts[sender_dict[msg[1]]].append(msg[2])
-    return sender_texts
+            current_line = " " + line.strip("\n")
+            chat[-1][3] += current_line
+            sender_messages[chat[-1][2]][-1] += current_line
+    chat_df = pd.DataFrame (chat, columns = ["date", "time", "sender", "message"])
+    chat_df['date'] = pd.to_datetime(chat_df['date'], infer_datetime_format=True)
+    return chat_df, sender_messages
