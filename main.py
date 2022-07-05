b7628b103171a54b2203d10aa2d6a5b367726091
Yunus R <73846253+yunvs@users.noreply.github.com>
2022-07-05 09:43:58 +0200

    added output information

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
+import pandas as pd
 from myFunctions import *
 from textStyle import *
-import pandas as pd
 from timeit import default_timer as timer
 start = timer() 
 
 paths = ["data/chat_small.txt", "data/chat_mum.txt",
 		"data/chat_mum.zip", "tash.trash", "data/chat_AB.txt"]
-path_to_file = paths[1] # 0: small, 1: mum.txt, 2: mum.zip, 3: trash, 4: AB
+path_to_file = paths[0] # 0: small, 1: mum.txt, 2: mum.zip, 3: trash, 4: AB
 
 
 contents = get_content(path_to_file) # get the content of the filepath
@@ -18,17 +18,17 @@ sender0, sender1 = str(), str() # initialize the senders
 sender0_df, sender1_df = pd.DataFrame(), pd.DataFrame() # initialize the dataframes
 
 
-# Use if there are more than two senders in the chat
-for i, s in enumerate(sender_dict):
-	globals()[f"sender{i}"] = senders[i] 
-	globals()[f"sender{str(i)}_df"] = pd.DataFrame(sender_dict[s], columns = [str(s)]) # create a dataframe for each sender
+
+for i, s in enumerate(senders):
+	globals()[f"sender{i}"] = senders[i] # sets variables the diffrent senders
+	globals()[f"sender{str(i)}_df"] = pd.DataFrame(sender_dict[s], columns = [s]) # create a dataframe for each sender
 
 
 
-# # Exporting for testing purposes
-# chat_df.to_csv("data/chat_df.csv", index=False) # save the dataframe to a csv file
-# sender0_df.to_csv("data/sender0_df.csv", index=False) # save the dataframe to a csv file
-# sender1_df.to_csv("data/sender1_df.csv", index=False) # save the dataframe to a csv file
+# # # Exporting for testing purposes
+chat_df.to_csv("data/testing/chat_df.csv", index=False) # save the dataframe to a csv file
+sender0_df.to_csv("data/testing/sender0_df.csv", index=False) # save the dataframe to a csv file
+sender1_df.to_csv("data/testing/sender1_df.csv", index=False) # save the dataframe to a csv file
 
 
 # print()
@@ -42,15 +42,16 @@ for i, s in enumerate(senders):
 	messages_df, stats = cleanse_df(globals()[f"sender{i}_df"]) # get the stats for each sender
 	msg_tot, msg_avg, msg_max, msg_min = get_stats(messages_df)
 
-
 	print(f"""
-	{GREEN(BLINK(f"WA-Report for {BOLD(s)}"))}:
-	{s} sent a total amound of {BOLD(f"{msg_tot} messages")} in this chat.
-	The average length of a message, written by {s}, is {BOLD(f"{msg_avg} characters")}, 
-	     the longest message, {s} sent, contained {BOLD(f"{msg_max} characters")}.
-	{s} sent {BOLD(f'{stats["audio"]} audio messages, {stats["image"]} images, {stats["video"]} videos and {stats["sticker"]} stickers')}.
-	# {s} shared {BOLD(f'{stats["contact"]} contact cards, {"##"} locations and {"##"} documents')}.
-	# {s} changed their mind {BOLD(f'{"##"} times and deleted a message')}.""")
+	{GREEN(f'WA-Report for {BOLD(s)}')}:
+	{s} sent {BOLD(f'{msg_tot} messages')} and {BOLD(f'{stats["media"]} media')} in this chat.
+	The {BOLD('average length')} of {s} messages is {BOLD(f'{msg_avg} characters')}, 
+	The {BOLD('longest message')} they sent contained {BOLD(f'{msg_max} characters')}.
+	{s} sent {BOLD(f'{stats["audio"]} audios, {stats["image"]} images, {stats["video"]} videos, {stats["sticker"]} stickers')} and {BOLD(f'{stats["gif"]} GIFs')}.
+	They shared {BOLD(f'{stats["contact"]} contacts, {stats["location"]} locations')} and {BOLD(f'{stats["document"]} documents')}.
+	{s} changed their mind {BOLD(f'{stats["deleted"]} times')} and {STRIKE(f'deleted a message')}.""")
+	if stats["missed"] > 0:
+		print(f"""	{BOLD(f'{stats["missed"]} (video)calls')} by {s} were missed.""")
 
 
 
