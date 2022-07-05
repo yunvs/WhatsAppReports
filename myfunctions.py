dfb38683048e1be60c63d3a1f9953b2151ea70c4
Yunus R <73846253+yunvs@users.noreply.github.com>
2022-07-05 12:29:41 +0200

    updated cleanse_df func. to be not repetetitive

--- myfunctions.py
+++ myfunctions.py
@@ -39,26 +39,27 @@ def convert_chat(wa_listed: list):
 	"""
 	chat = list() # list of all messages
 	sender_messages = dict() # dictionary with key: sender, value: list of messages
-	matched_before = False 
+	matched = False 
 
 	for line in wa_listed:
 		if re.match(PATTERN, line): # check if a new message starts in line
+			matched = True
 			res = re.search(PATTERN, line) # get groups of line
 			# groups are: 1:date, 2:time, 3:sender, 4:message
-			chat.append([res.group(1), res.group(2), res.group(3), res.group(4)])
-			matched_before = True
-			if res.group(3) not in sender_messages:
-				sender_messages[res.group(3)] = [res.group(4)]
+			sender = res.group(3).split()[0].capitalize() # get sender
+			chat.append([res.group(1), res.group(2), sender, res.group(4)])
+			if sender not in sender_messages:
+				sender_messages[sender] = [res.group(4)]
 			else:
-				sender_messages[res.group(3)].append(res.group(4))
+				sender_messages[sender].append(res.group(4))
 		else:
-			if not matched_before:
+			if not matched:
 				sys.exit(BOLD(RED("ERROR: Sorry, the chat is not in the correct format")))
 			current_line = " " + line.strip("\n")
 			chat[-1][3] += current_line
 			sender_messages[chat[-1][2]][-1] += current_line
 	chat_df = pd.DataFrame (chat, columns = ["date", "time", "sender", "message"])
-	chat_df["date"] = pd.to_datetime(chat_df["date"], infer_datetime_format=True)
+	chat_df["date"] = pd.to_datetime(chat_df["date"],infer_datetime_format=True)
 	return chat_df, sender_messages
 
 
@@ -87,47 +88,40 @@ def cleanse_df(sender_df: pd.DataFrame):
 	s_clean = sender_df.copy()
 	sender = s_clean.columns[0] # current sender
 
+	entities = {"ximg":"‎image omitted", "xaud":"‎audio omitted", 
+		"xstick":"‎sticker omitted", "xvid":"‎video omitted", 
+		"xgif":"‎GIF omitted", "med":"", "xmiss":"‎Missed ", 
+		"xcont":"‎Contact card omitted", "xloc":"‎Location: ", 
+		"xdoc":"‎document omitted", 
+		"xdel":"(‎You deleted |‎This message was deleted.)"}
+
 	# Extract and remove non-message enties
-	image_df = s_clean[s_clean[sender] == "‎image omitted"]
-	s_clean = s_clean.drop(image_df.index) # remove image messages
-	audio_df = s_clean[s_clean[sender] == "‎audio omitted"]
-	s_clean = s_clean.drop(audio_df.index) # remove audio messages
-	sticker_df = s_clean[s_clean[sender] == "‎sticker omitted"]
-	s_clean = s_clean.drop(sticker_df.index) # remove sticker messages
-	video_df = s_clean[s_clean[sender] == "‎video omitted"]
-	s_clean = s_clean.drop(video_df.index) # remove video messages
-	gif_df = s_clean[s_clean[sender] == "‎GIF omitted"]
-	s_clean = s_clean.drop(gif_df.index) # remove GIF messages
-	missed_df = s_clean[s_clean[sender].str.startswith("‎Missed ")]
-	s_clean = s_clean.drop(missed_df.index) # remove missed messages
-	contact_df = s_clean[s_clean[sender] == "‎Contact card omitted"]
-	s_clean = s_clean.drop(contact_df.index) # remove contact messages
-	location_df = s_clean[s_clean[sender].str.startswith("‎Location: https://maps.google.com/")]
-	s_clean = s_clean.drop(location_df.index) # remove location messages
-	document_df = s_clean[s_clean[sender].str.endswith("‎document omitted")]
-	s_clean = s_clean.drop(document_df.index) # remove document messages
-	deleted_df = s_clean[s_clean[sender].str.startswith(("‎You deleted this message.", "‎This message was deleted."))]
-	s_clean = s_clean.drop(deleted_df.index) # remove "deleted" messages
+	for key, value in entities.items():
+		if key != "med":
+			if key not in ("xmiss", "xloc", "xdoc", "xdel"):
+				key_df = s_clean[s_clean[sender] == value]
+			else:
+				key_df = s_clean[s_clean[sender].str.contains(value)]
+		if key.startswith("x"):
+			s_clean = s_clean.drop(key_df.index)
+		globals()[f"{key}_df"] = key_df
+
 
 	# remove all remaining non-message enties (Whatsapp system messages)
 	s_clean = s_clean.drop(s_clean[s_clean[sender].str.contains("‎")].index)
 
 	# Testing purposes only
-	sender_df.to_csv(f"data/testing/myFunctions/sender_df_{sender}.csv", index=True) # save the dataframe to a csv file
-	s_clean.to_csv(f"data/testing/myFunctions/s_clean_{sender}.csv", index=True) # save the dataframe to a csv file
-
+	sender_df.to_csv(f"data/testing/myFunctions/sender_df_{sender}.csv",index=True) # save the dataframe to a csv file
+	s_clean.to_csv(f"data/testing/myFunctions/s_clean_{sender}.csv",index=True) # save the dataframe to a csv file
 	
-	# enter the extracted counts into the stats
-	stats["image"] = image_df.shape[0]
-	stats["audio"] = audio_df.shape[0]
-	stats["sticker"] = sticker_df.shape[0]
-	stats["video"] = video_df.shape[0]
-	stats["gif"] = gif_df.shape[0]
-	stats["document"] = document_df.shape[0]
-	stats["media"] = sum(stats.values())
-	stats["contact"] = contact_df.shape[0]
-	stats["location"] = location_df.shape[0]
-	stats["missed"] = missed_df.shape[0]
-	stats["deleted"] = deleted_df.shape[0]
+	# Get stats about message enties
+	# link_df = s_clean[s_clean[sender].str.contains(("http://","https://"),case=False,regex=True)]
 
+	# enter the extracted counts into the stats
+	for key in entities.keys():
+		if key != "med":
+			stats[key] = globals()[f"{key}_df"].shape[0] # get the amount of non-message enties
+		else:
+			stats[key] = sum(stats.values()) 
+	
 	return s_clean, stats # return the cleaned dataframe and the stats dict
