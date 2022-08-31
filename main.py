7aadf4deaf89af28317b80d5bbf961bb1efd6f05
Yunus <73846253+yunvs@users.noreply.github.com>
2022-08-31 12:25:39 +0200

    implemented new functions and statistics

--- main.py
+++ main.py
@@ -25,27 +25,26 @@ prep_db()
 
 # data seperation, cleansing and data analysis per sender
 for i, s in enumerate(db.senders):
-	# bar.text = f"Cleaning and analyzing messages from {s}"
-	# globals()[f"s{i}"] = s
-	df = db.chat.loc[db.chat["sender"] == s, "message", ]  # dataframe with messages from sender
-	db.chat_p_sender.append(df)
-	clean_df = cleanse_df(df, s)  # get the stats for each sender
-	calc_stats(clean_df)  # set the cleaned dataframe to the global variable
+	# bar.text = f"Cleaning and analyzing messages from {s}" #REMOVE
+	df = db.chat.loc[db.chat["sender"] == s]  # dataframe with messages from sender
+	db.chat_per_s.append(df)
+	clean_df = cleanse_df(df["message"], s)  # get the stats for each sender
+	calc_stats(clean_df) # calculate the stats for each sender
 	# globals()[f"s{i}_df_clean"] = clean_df
-	word_freq = calc_word_stats(clean_df)
+	word_freq = calc_word_stats(clean_df, i)
 	# globals()[f"s{i}_word_freq"] = word_freq
 
-db.chat_p_sender.append(db.chat_og)
+db.chat_per_s.append(db.chat_og)
 
 # bar.text = "Calculating statistics"
 calc_sum_stats()  # get the summary statistics for all senders
+calc_time_stats()  # get the time stats for the chat
 
 # bar.text = "Creating plots and tables"
 make_plots()
 
 db.reports = get_txt_reports()  # get general sender stats for the chat
 
-calc_time_stats()  # get the time stats for the chat
 
 # bar.text = "Creating and saving report @ data/output/pdfs/"
 make_pdf_report()  # create the pdf report
