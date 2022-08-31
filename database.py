cf43eefe93f13bc60e55323de7b3540e7e1d7684
Yunus <73846253+yunvs@users.noreply.github.com>
2022-08-31 10:56:17 +0200

    database extended

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 from nltk.corpus import stopwords
-stopset = stopwords.words("german")
+stop_words = stopwords.words("german")
 
 
 # ----------------------------------------------------------------
@@ -43,7 +43,7 @@ stop_words = ["ich", "du", "und", "der", "die", "das", "ist", "nicht", "sich", "
 
 plot1_dict = {7: "Images", 8: "Videos", 9: "GIFs", 10: "Stickers", 11: "Audios", 12: "Contacts", 13: "Locations", 14: "Files"}
 
-time_stats_cols = ["first_msg", "last_mgs", "most_msg", "no_msg", "break"]
+time_stats_cols = ["first_msg", "last_msg", "zero_days", "most_msg", "break"]
 
 
 
@@ -61,16 +61,26 @@ chat = list()
 senders: list[str] = list()
 """ #tobefilled: list of all senders """
 
-sc: int = 0
+sc: int = len(senders)
 """ #tobefilled: count of senders """
 
-chat_p_sender = list()
+sender_chats = list()
+""" #tobefilled: Chat per sender [0: chat from sender no.1, 1: chat from sender no.2, ... sc: chat from all senders] """
+
+sender_chats_clean = list()
+""" #tobefilled: clean chat per sender [0: chat from sender no.1, 1: chat from sender no.2, ... sc: chat from all senders] """
+
+word_freqs = list()
+""" #tobefilled: word frequencies per sender [0: chat from sender no.1, 1: chat from sender no.2, ...] """
 
 stats_df = list()
 """ #tobefilled: 0: msg_count, 1: chars_avg, 2: words_avg, 3: chars_max, 4: words_max, 5: polarity_avg, 6: media_count, 7: image, 8: video, 9: GIF, 10: sticker, 11: audio, 12: contact, 13: location, 14: file, 15: deleted, 16: calls, 17: link, 18: emoji, 19: emoji_unique """
 
 time_stats_df = list()
 """ #tobefilled: 0: first_msg, 1: last_mgs, 2: most_msg, 3: no_msg, 4: break """
+
+msg_ranges = list()
+""" #tobefilled: [start, end] """
 
 reports: list[str] = list()
 """ #tobefilled: list contains text reports (general and per sender) """
