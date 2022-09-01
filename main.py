7a40c36dda4da3f3aad3293b22b1d7ba7a3d0386
Yunus <73846253+yunvs@users.noreply.github.com>
2022-09-01 13:47:47 +0200

    new seperate_data func, old test variables,

--- main.py
+++ main.py
@@ -3,38 +3,18 @@
 # with alive_bar(monitor=False, stats=False) as bar:
 # bar.text = "Importing modules"
 
-print("\n\nmain.py started")
+print("\n\nmain.py started\n")
 from myfuncs import *
-
-# Only Testing # Initializing Variables for autocomplete ###
-s0, s1 = str(), str()
-s0_df, s1_df = pd.DataFrame(), pd.DataFrame()
-s0_df_clean, s1_df_clean = pd.DataFrame(), pd.DataFrame()
-#### Only Testing ####
-
 
 path = db.test_data[1]  # 0: small, 1: txt, 2: zip, 3: trash, 4: AB, 5: _chat.txt
 
 # bar.text = "Importing and converting data"
 
-
 # data extraction and preprocessing
 convert_to_df(fileformat(path))  # convert the chat to a pandas dataframe
-prep_db()
+prepare_db()
 
-
-# data seperation, cleansing and data analysis per sender
-for i, s in enumerate(db.senders):
-	# bar.text = f"Cleaning and analyzing messages from {s}" #REMOVE
-	df = db.chat.loc[db.chat["sender"] == s]  # dataframe with messages from sender
-	db.chat_per_s.append(df)
-	clean_df = cleanse_df(df["message"], s)  # get the stats for each sender
-	calc_stats(clean_df) # calculate the stats for each sender
-	# globals()[f"s{i}_df_clean"] = clean_df
-	word_freq = calc_word_stats(clean_df, i)
-	# globals()[f"s{i}_word_freq"] = word_freq
-
-db.chat_per_s.append(db.chat_og)
+seperate_data()
 
 # bar.text = "Calculating statistics"
 calc_sum_stats()  # get the summary statistics for all senders
