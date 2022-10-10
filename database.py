01e1474d2cbfdbec790a3b2c0a93d919c8743be4
Yunus <73846253+yunvs@users.noreply.github.com>
2022-10-10 17:53:12 +0300

    changed wording and added comments

--- database.py
+++ database.py
@@ -6,24 +6,27 @@ from nltk.corpus import stopwords
 # which will be derived from the chat
 # ----------------------------------------------------------------
 
-test_data = ["data/input/chat_small.txt", "data/input/chat_mum.txt", "data/input/chat_mum.zip", "data/input/tash.trash", "data/input/chat_AB.txt", "data/input/_chat.txt"]
+test_data = ["data/input/chat_small.txt", "data/input/chat_mum.txt", "data/input/chat_mum.zip", "data/input/tash.trash", "data/input/chat_AB.txt", "data/input/_chat.txt"] #TODO remove finally
+""" List of paths to test data 1: small, 2: txt, 3: zip, 4: trash, 5: AB, 6: _chat.txt """
 
-df_cols = ["date", "datetime", "sender", "message", "polarity"]
+df_cols = ["date", "datetime", "sender", "message", "sentiment"]
+""" List of column names for the DataFrame """
 
 stats_dict = {
-	"msg_count": "message", 			"chars_avg": "char", 
-	"words_avg": "word", 				"chars_max": "char", 
-	"words_max": "word", 				"polarity_avg": "polarity", 
-	"media_count": "media", 			"image": "image", 
-	"video": "video", 					"GIF": "GIF", 
-	"sticker": "sticker", 				"audio": "audio", 
-	"contact": "contact", 				"location": "location", 
-	"file": "file", 					"deleted": "message", 
-	"calls": "video or audio call",		"link": "link", 
-	"emoji": "emoji", 					"emoji_unique": "emoji",
-	"polarity_pos": "positve message",	"polarity_neu": "neutral message",
-	"polarity_neg": "negative message",	"polarity_nan": "message",}
-""" .keys() = [msg_count, chars_avg, words_avg, chars_max, words_max, polarity_avg, media_count, image, video, GIF, sticker, audio, contact, location, file, deleted, calls, link, emoji, emoji_unique, polarity_pos, polarity_neu, polarity_neg, polarity_nan] """
+	"msg_count": "message", 		"chars_avg": "char", 
+	"words_avg": "word", 			"chars_max": "char", 
+	"words_max": "word", 			"sent_avg": "sentiment", 
+	"media_count": "media", 		"image": "image", 
+	"video": "video", 				"GIF": "GIF", 
+	"sticker": "sticker", 			"audio": "audio", 
+	"contact": "contact", 			"location": "location", 
+	"file": "file", 				"deleted": "message", 
+	"calls": "video/audio call",	"link": "link", 
+	"emoji": "emoji", 				"emoji_unique": "emoji",
+	"sent_pos": "positve message",	"sent_neu": "neutral message",
+	"sent_neg": "negative message",	"sent_nan": "message",}
+""" .keys() = [msg_count, chars_avg, words_avg, chars_max, words_max, sent_avg, media_count, image, video, GIF, sticker, audio, contact, location, file, deleted, calls, link, emoji, emoji_unique, sent_pos, sent_neu, sent_neg, sent_nan] """ #TODO remove finally
+""" Dictionary of statistics and their corresponding names """
 
 
 cstats_match: dict[str, tuple[str, str]] = {
@@ -39,24 +42,28 @@ cstats_match: dict[str, tuple[str, str]] = {
 	"calls": ("", "‎Missed "),
 	"deleted": ("", "‎You deleted |‎This message was deleted."),
 	"rest": ("", "‎")}
+""" Dictionary of media types and their corresponding patterns """
 
 stop_words = ["ich", "du", "und", "der", "die", "das", "ist", "nicht", "sich", "mit", "wie", "an", "auf", "aus", "bei", "bis", "bist", "da", "dann", "das", "dass", "dein", "deine", "dem", "den", "der", "des", "dessen", "dich", "dir", "doch", "dort", "durch", "ein", "eine", "einem", "einen", "einer", "eines", "einmal", "er", "es", "etwas", "für", "gegen", "habe", "haben", "hat", "hatte", "hatten", "hier", "hin", "hinter", "ich", "ihr", "ihre", "im", "in", "indem", "ins", "ist", "jede", "jedem", "jeden", "jeder", "jedes", "jene", "jenem", "jenen", "jener", "jenes", "jetzt", "kann", "kannst", "können", "könnt", "machen", "mein", "meine", "meinst", "meinst", "mich", "mir", "mit", "muss", "müssen", "musst", "nach", "nicht", "nichts", "noch", "nun", "nur", "ob", "oder", "ohne", "seid", "seien", "seinem", "sein", "seine", "seinst", "seinst", "sich", "sie", "sind", "soll", "sollen", "sollst", "sollt", "sollte", "sollten", "solltest", "sonst", "soweit", "über", "überhaupt", "übrigens", "unter", "viel", "viele", "vom", "von", "vor", "wann", "war", "waren", "warst", "wart", "warum", "was", "weg", "wegen", "weil"]
-stop_words = set(stopwords.words('german') + stop_words)
+stop_words = set(stopwords.words("german") + stop_words)
 stop_words = [word.upper() for word in stop_words]
+""" List of stopwords for the wordclouds """
 
 plot1_dict = {7: "Images", 8: "Videos", 9: "GIFs", 10: "Stickers", 11: "Audios", 12: "Contacts", 13: "Locations", 14: "Files"}
+""" Dictionary of labels for the first plot """
 
 time_stats_cols = ["first_msg", "last_msg", "zero_days"]
+""" List of column names for the time statistics DataFrame """
 
 # ----------------------------------------------------------------
 # the actual data will be stored in the following variables
 # ----------------------------------------------------------------
 
 chat_og = pd.DataFrame()
-""" #tobefilled: [date, datetime, sender, message, polarity, emojis, emoji_count, url_count] """
+""" #tobefilled: [date, datetime, sender, message, sentiment] """
 
 chat = pd.DataFrame()
-""" #tobefilled: [date, datetime, sender, message, polarity, emojis, emoji_count, url_count] """
+""" #tobefilled: [date, datetime, sender, message, sentiment] """
 
 senders: list[str] = list()
 """ #tobefilled: list of all senders """
