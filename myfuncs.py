d29e0641014d392162eec591a003ec915d0adb04
Yunus <73846253+yunvs@users.noreply.github.com>
2022-10-18 13:48:36 +0200

    adapted text and time reports

--- myfuncs.py
+++ myfuncs.py
@@ -357,17 +357,15 @@ def create_txt_reports() -> None:
 
 	def y(*idxs): return pprint(*[calc_num(*get_stat_pair(a, i)) for a in idxs])
 
-	def x(*idxs): return pprint(*[db.stats.iat[i, a] for a in idxs])
 
-	for i, s in enumerate(db.senders):  # for each sender create a sender report
+	for i, s in enumerate(db.sender):  # for each sender create a sender report
 		ls = [y(0, 6, 18), "\n".join([
 			f"Avg. message length: {y(2)} ({y(1)})",
 			f"Longest message: {y(4)} ({y(3)})",
-			f"{x(15)} deleted messages",
-			f"{s} deleted {y(15)}.",
-			])]
+			f"{y(15)} were deleted by {s}."])]
 		if db.stats.iat[i, 16] != 0:
-			ls.append(f"You missed {y(16)} by {s}.") #TODO check this
+			ls[-1] += (f"\n{y(16)} by {s} were missed.")
+		ls.append(f"In total {s} used {len(db.common_words[i])} distinct words and {len(db.common_emojis[i])} distinct emojis.")
 		db.txt_reports.append(ls)
 
 	# General report about all chat messages
@@ -375,8 +373,7 @@ def create_txt_reports() -> None:
 	db.txt_reports.append([y(0, 6, 18), "\n".join([
 		f"Avg. message length: {y(2)} ({y(1)})",
 		f"Longest message: {y(4)} ({y(3)})",
-		f"{x(15)} deleted messages",
-		f"{ss()} deleted {y(15)}."])])
+		f"{ss()} deleted {y(15)}.",])])
 	return
 
 
@@ -385,13 +382,23 @@ def create_time_reports() -> None:
 	Creates a report about the sender at the given index which includes the
 	statistics about their messages or a summary report.
 	"""
-	for i in range(db.sc+1):  # for each sender create a sender report 
-		db.time_reports.append("\n".join([
+	for i, s in enumerate(db.sender):  # for each sender create a sender report
+		db.time_reports.append([
 			f"First message: {db.tstats.iat[i, 0]}",
+			f"Most messages: {db.tstats.iat[i, 2]} ({db.tstats.iat[i, 3]} msg.)",
 			f"Last message: {db.tstats.iat[i, 1]}",
-			f"Most messages sent on {db.tstats.iat[i, 2]} ({db.tstats.iat[i, 3]})",
-			f"Messages sent on {db.tstats.iat[i, 4]} days",
-			f"No messages sent on {db.tstats.iat[i, 5]} days"]))
+			f"{s} sent at lest one message on {db.tstats.iat[i, 4]} days and no messages on {db.tstats.iat[i, 5]} days.",
+			f"The longest period without any messages from {s} was {int(db.tstats.iat[i, 7])} days.",
+			])
+
+	db.time_reports.append([
+		f"First message: {db.tstats.iat[i, 0]}",
+		f"Most messages: {db.tstats.iat[i, 2]} ({db.tstats.iat[i, 3]} msg.)",
+		f"Last message: {db.tstats.iat[i, 1]}",
+		f"At lest one message sent on {db.tstats.iat[i, 4]} days.",
+		f"Not a single message sent on {db.tstats.iat[i, 5]} days.",
+		f"Longest period without any messages: {int(db.tstats.iat[i, 7])} days.",
+		])
 	return
 
 
