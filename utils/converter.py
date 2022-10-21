40bdcd26ad5018114077c82ca6a8abb10301bb1f
Yunus <73846253+yunvs@users.noreply.github.com>
2022-10-21 02:42:13 +0200

    moved functionalities from converter to getter

--- utils/converter.py
+++ utils/converter.py
@@ -4,38 +4,39 @@ import re  # for regex
 from textblob_de import TextBlobDE as TextBlob  # for sentiment analysis
 
 
-def convert_file(path: str) -> None:
+def convert_lines(lines: str) -> None:
 	"""
 	Checks and converts a file of messages into a pandas DataFrame with the columns date, datetime, sender, message and sentiment.
 	"""
-	chat_list = convert_file_to_list(path)
+	chat_list = convert_list(lines)
 	v.chat = convert_list_to_df(chat_list)
 
-	time("converting the original file to a pandas DataFrame")
+	time("Converting the original file to a pandas DataFrame")
 	return
 
 
-def convert_file_to_list(path_to_file: str) -> list[list[str]]:
+def convert_list(lines: list[str]) -> list[list[str]]:
 	"""
-	Converts a .txt-file of messages into a list of messages and returns it.
+	Converts a list of strings into a list of sorted messages and returns it.
 	"""
 	chat, last_msg = list(), str()
-	try:  # try to open the file
-		with open(path_to_file, "r", encoding="utf-8") as file:
-			for line in file:  # read line by line
-				# line begins with a new message
-				if re.match("^.? ?\[([\d./]*), ([\d:]*)\] .*", line):
-					# add buffer to list
-					chat.append(convert_ln(last_msg)) if last_msg != "" else None
-					last_msg = line.strip().strip("\n").strip()  # add new message to buffer
-				else:  # line is the continuation of the last message
-					last_msg += " " + line.strip().strip("\n").strip()  # add line to buffer
-			# add buffer to list if it is not added yet
-			chat.append(convert_ln(last_msg)) if chat[-1] != convert_ln(last_msg) else None
-	except FileNotFoundError:  # file does not exist
-		return off("File not found\nCheck the path to the file")
-	except (UnicodeDecodeError, UnicodeError):  # file is not in UTF-8
-		return off("File not in UTF-8 format\nTry uploading the original file")
+	first_msg_reached = False
+	for line in lines[1:]:  # iterate over lines
+		if not first_msg_reached:
+			if not re.match("^.? ?\[([\d./]*), ([\d:]*)\] .*:.*$", line):
+				continue
+			else:
+				first_msg_reached = True
+		# line begins with a new message
+		if re.match("^.? ?\[([\d./]*), ([\d:]*)\] .*:.*$", line):
+			# add buffer to list
+			chat.append(convert_ln(last_msg)) if last_msg != "" else None
+			last_msg = line.strip().strip("\n").strip()  # add new message to buffer
+		else:  # line is the continuation of the last message
+			last_msg += " " + line.strip().strip("\n").strip()  # add line to buffer
+	# add buffer to list if it is not added yet
+	chat.append(convert_ln(last_msg)) if chat[-1] != convert_ln(last_msg) else None
+
 	return chat
 
 
@@ -44,9 +45,9 @@ def convert_ln(input: str) -> list[str]:
 	Converts a line of the WhatsApp chat history into a list and returns it.
 	"""
 	# match pattern and divide into groups: 1:date, 2:datetime, 3:sender, 4:message
-	x = re.search("^.? ?\[([\d./]*), ([\d:]*)\] ([\w ]*): (\u200E?.*)$", input)
+	x = re.search("^.? ?\[([\d./]*), ([\d:]*)\] (.+?\u202A?): (\u200E?.*)$", input)
 	result = [x.group(1), " ".join([x.group(1), x.group(2)])]  # add date, datetime
-	result.append(x.group(3).title())  # add name of sender
+	result.append(x.group(3).strip("‪").strip("‬").title())  # add name of sender
 	message = re.sub("https?://\S+", "xURLx", x.group(4))  # replace URLs
 	result.append(message)  # add message
 	if len(message) > 2 and message.upper().isupper():  # add sentient
